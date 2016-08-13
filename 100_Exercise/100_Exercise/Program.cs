9ea3dc5da48fd135a4ffa5d4e56a8098d7a83651
dmitriyt <githubx-treme.su>
2016-08-13 19:12:49 +0300

    Вычисление факториала без рекурсии

--- 100_Exercise/100_Exercise/Program.cs
+++ 100_Exercise/100_Exercise/Program.cs
@@ -7,9 +7,62 @@ using System.Threading.Tasks;
 namespace _100_Exercise
 {
     class Program
+    
     {
         static void Main(string[] args)
         {
+            string _input_str = null;
+            int _input_int=0;
+            
+            Console.WriteLine("Enter a natural number from 1 to 200. For stop programm press CTRL+Z");
+            
+            do
+            {
+                Console.Write("Number from 1 to 200 >> ");
+                _input_str = System.Console.ReadLine();
+                if (_input_str !=null)
+                try
+                {
+                    _input_int = Int32.Parse(_input_str);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+
+                if (_input_int >= 0 && _input_int <= 200)
+                {
+                    int calculation = FactorialCalculation(_input_int);
+                    Console.WriteLine("Result of calculation {0:N}! is {1:N}", _input_str, calculation.ToString());
+                    
+                }
+                else
+                {
+                    Console.WriteLine("You entered an incorrect value.");
+                }
+                Console.WriteLine("");
+            } while (_input_str != null);
+
+
         }
+
+        static private int FactorialCalculation (int _number)
+        {
+            int result = 1;
+            if (_number > 1)
+            {
+                for (int i=2; i <= _number; i++)
+                {
+                    result = result * i;
+                }   
+            }
+            else
+            {
+                result = 1;
+            }
+            return result;
+
+        } 
     }
 }
---
+++ 100_Exercise/MathOperations.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class MathOperations
+{
+	public MathOperations()
+	{
+
+	}
+}
